166a9b8f5c46bd089b09c7d31dac5fad37fbfe38
szczepaniakmichal <mssk87@gmail.com>
2019-11-19 19:20:20 +0100

    all works fine :)

--- script/script.js
+++ script/script.js
@@ -1,16 +1,24 @@
-const createElements = () => {
+let count = 0;
+let fontSize = 10;
 
+const createElements = () => {
+    const ul = document.querySelector('ul');
+    for (let i = 0; i < 5; i++) {
+        count++;
+        const li = document.createElement('li');
+        li.textContent = `Element nr ${count}`;
+        li.style.fontSize = `${fontSize}px`;
+        ul.appendChild(li);
+        fontSize++
+    }
 };
 
 const init = () => {
     const btn = document.createElement('button');
     btn.textContent = 'Stwórz 4 elementów';
-
     const listOfElements = document.createElement('ul');
-
     document.body.appendChild(btn);
     document.body.appendChild(listOfElements);
-
     btn.addEventListener('click', createElements);
 };
 
