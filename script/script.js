c5773465cf8c2e93dcc1c6cd9cdb6ab420710bde
szczepaniakmichal <mssk87@gmail.com>
2019-11-19 21:41:13 +0100

    add random font color, add remover button,

--- script/script.js
+++ script/script.js
@@ -1,5 +1,16 @@
 let count = 0;
 let fontSize = 10;
+let fontColor = '';
+const listOfElements = document.createElement('ul');
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 const createElements = () => {
     const ul = document.querySelector('ul');
@@ -8,18 +19,31 @@ const createElements = () => {
         const li = document.createElement('li');
         li.textContent = `Element nr ${count}`;
         li.style.fontSize = `${fontSize}px`;
+        fontColor = getRandomColor();
+        li.style.color = fontColor;
         ul.appendChild(li);
         fontSize++
     }
 };
 
+const clearList = () => {
+    count = 0;
+    fontSize = 10;
+    while (listOfElements.firstChild) {
+        listOfElements.firstChild.remove();
+    }
+};
+
 const init = () => {
     const btn = document.createElement('button');
-    btn.textContent = 'Stwórz 4 elementów';
-    const listOfElements = document.createElement('ul');
+        btn.textContent = 'Stwórz 4 elementów';
+        btn.addEventListener('click', createElements);
+    const btnClear = document.createElement('button');
+        btnClear.textContent = 'wyczyść listę';
+        btnClear.addEventListener('click', clearList);
     document.body.appendChild(btn);
+    document.body.appendChild(btnClear);
     document.body.appendChild(listOfElements);
-    btn.addEventListener('click', createElements);
 };
 
 init();
